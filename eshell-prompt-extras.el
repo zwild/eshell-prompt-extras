7581c109673c40aceff278cd524273f50ffe170d
Chunyang Xu <mail@xuchunyang.me>
2017-09-15 10:55:11 +0800

    * eshell-prompt-extras.el (epe-remote-host): Fix typo.

--- eshell-prompt-extras.el
+++ eshell-prompt-extras.el
@@ -241,7 +241,7 @@ length of PATH (sans directory slashes) down to MAX-LEN."
   ;; `tramp-file-name-real-host' is removed and replaced by
   ;; `tramp-file-name-host' in Emacs 26, see issue #18
   (if (fboundp 'tramp-file-name-real-host)
-      (tramp-file-name-host (tramp-dissect-file-name default-directory))
+      (tramp-file-name-real-host (tramp-dissect-file-name default-directory))
     (tramp-file-name-host (tramp-dissect-file-name default-directory))))
 
 
